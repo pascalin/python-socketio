5af917ff99473a9bcd76acc9de53892cfc055af1
David Suarez Pascal <david@apuntia.com>
2022-09-20 12:34:40 -0500

    Adapted WSGI app example to use the Pyramid web framework.

---
+++ examples/server/wsgi/pyramid_app.py
@@ -0,0 +1,119 @@
+# set async_mode to 'threading', 'eventlet', 'gevent' or 'gevent_uwsgi' to
+# force a mode else, the best mode is selected automatically from what's
+# installed
+async_mode = None
+
+
+from waitress import serve
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.renderers import render_to_response
+import socketio
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        sio.sleep(10)
+        count += 1
+        sio.emit('my_response', {'data': 'Server generated event'})
+
+
+def index(request):
+    global thread
+    if thread is None:
+        thread = sio.start_background_task(background_thread)
+    return {}
+
+
+sio = socketio.Server(logger=True, async_mode=async_mode)
+with Configurator() as config:
+    config.include('pyramid_jinja2')
+    config.add_route('index', '/')
+    config.add_view(index, route_name='index', renderer='templates/index.jinja2')
+    wsgi_app = config.make_wsgi_app()
+app = socketio.WSGIApp(sio, wsgi_app)
+thread = None
+
+
+@sio.event
+def my_event(sid, message):
+    sio.emit('my_response', {'data': message['data']}, room=sid)
+
+
+@sio.event
+def my_broadcast_event(sid, message):
+    sio.emit('my_response', {'data': message['data']})
+
+
+@sio.event
+def join(sid, message):
+    sio.enter_room(sid, message['room'])
+    sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
+             room=sid)
+
+
+@sio.event
+def leave(sid, message):
+    sio.leave_room(sid, message['room'])
+    sio.emit('my_response', {'data': 'Left room: ' + message['room']},
+             room=sid)
+
+
+@sio.event
+def close_room(sid, message):
+    sio.emit('my_response',
+             {'data': 'Room ' + message['room'] + ' is closing.'},
+             room=message['room'])
+    sio.close_room(message['room'])
+
+
+@sio.event
+def my_room_event(sid, message):
+    sio.emit('my_response', {'data': message['data']}, room=message['room'])
+
+
+@sio.event
+def disconnect_request(sid):
+    sio.disconnect(sid)
+
+
+@sio.event
+def connect(sid, environ):
+    sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
+
+
+@sio.event
+def disconnect(sid):
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    if sio.async_mode == 'threading':
+        # deploy with Waitress
+        serve(app, host='0.0.0.0', port=6543)
+    elif sio.async_mode == 'eventlet':
+        # deploy with eventlet
+        import eventlet
+        import eventlet.wsgi
+        eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    elif sio.async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    elif sio.async_mode == 'gevent_uwsgi':
+        print('Start the application through the uwsgi server. Example:')
+        print('uwsgi --http :5000 --gevent 1000 --http-websockets --master '
+              '--wsgi-file pyramid_app.py --callable app')
+    else:
+        print('Unknown async_mode: ' + sio.async_mode)
